d298d0d7f8361b9a968ed458dd092d904bbdd7a6
Gitea <gitea@fake.local>
2021-04-01 18:44:31 +0300

    initial

---
+++ src/main/java/com/example/dataflow/DataFlowStartStartApplication.java
@@ -0,0 +1,30 @@
+package com.example.dataflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
+import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
+import org.springframework.cloud.deployer.spi.local.LocalDeployerAutoConfiguration;
+import org.springframework.cloud.kubernetes.KubernetesAutoConfiguration;
+
+@EnableDataFlowServer
+@SpringBootApplication(
+        exclude = {SessionAutoConfiguration.class,
+                ManagementWebSecurityAutoConfiguration.class,
+                SecurityAutoConfiguration.class,
+                UserDetailsServiceAutoConfiguration.class,
+                LocalDeployerAutoConfiguration.class,
+                KubernetesAutoConfiguration.class,
+        })
+public class DataFlowStartStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataFlowStartStartApplication.class, args);
+    }
+
+}
+
+
